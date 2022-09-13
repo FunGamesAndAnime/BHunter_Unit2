b6d6c6b3f7b844a4394efc8b6fbc70f8587771b0
FunGamesAndAnime <111932365+FunGamesAndAnime@users.noreply.github.com>
2022-09-13 16:26:31 -0700

    Unit 2: Lesson B

--- Assets/Course Library/scripts/PlayerController.cs
+++ Assets/Course Library/scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     private float xrange = 15.0f;
+
+    public GameObject projectialprefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +30,12 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x < -xrange)
         {
             transform.position = new Vector3(-xrange, transform.position.y, transform.position.z);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+         Instantiate(projectialprefab, transform.position, projectialprefab.transform.rotation);
         }
     }
 }
---
+++ Assets/Course Library/scripts/destory_out_of_bouns.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destory_out_of_bouns : MonoBehaviour
+{
+    private float topOfSceane = 30.0f;
+    private float bottomOfSceane = -15.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > topOfSceane)
+        {
+            Destroy(gameObject);
+        }
+        else if(transform.position.z < bottomOfSceane)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
